b987130f608d447af8c86eba47a6165b755d0b25
Letícia Santos da Silva <147554295+leticiasantossilva@users.noreply.github.com>
2024-01-25 01:59:41 -0300

    update es_test

--- Projeto 9/capm/es.py
+++ Projeto 9/capm/es.py
@@ -8,6 +8,7 @@ Data: 13/12/2023
 
 """
 
+# ENTRADA DE DADOS
 
 # Importando bibliotecas necessárias
 
@@ -17,30 +18,48 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
+import os
 
 
-# Defininido a função de leitura de preços de ações
+# Leitura de retornos de preços de ações
 
-def leitor_precos(ticker:str):
-    """" Esta função realiza a leitura de preço de fechamento de ações nas quarta-feiras de todas as últimas 260 semanas """
+def leitor_precos(ticker:str) -> np.array:
+    """" Esta função realiza a leitura de preço de fechamento de ações nas quarta-feiras de todas as últimas 260 semanas e calcula seus retornos """
     end_date = datetime.now()
-    start_date = end_date - timedelta(weeks=262)
+    start_date = end_date - timedelta(weeks=263)
     dado = yf.download(ticker, start=start_date, end=end_date, progress=False, interval='1d')
     data_day = dado[dado.index.weekday == 2]
-    fechamento = data_day['Adj Close'].dropna()
-    return fechamento
+    fechamento = data_day['Adj Close'].pct_change().dropna()
+    return np.array(fechamento)
 
 
-# Defininido a função de leitura da carteira de mercado (IBOVESPA)
+# Leitura de retornos da carteira de mercado (IBOVESPA)
 
-def leitor_indice():
-    """" Esta função realiza a leitura de fechamento do Bovespa nas quartas-feiras de todas as últimas 260 semanas """
+def leitor_indice() -> np.array:
+    """" Esta função realiza a leitura de fechamento do Bovespa nas quartas-feiras de todas as últimas 260 semanas e calcula seus retornos """
     end_date = datetime.now()
-    start_date = end_date - timedelta(weeks=262)
+    start_date = end_date - timedelta(weeks=263)
     bov = yf.download("^BVSP", start=start_date, end=end_date, progress=False, interval='1d')
-    fechamento = bov['Adj Close']
-    mercado = fechamento[fechamento.index.weekday == 2]
-    return mercado    
-    
+    retorno = bov['Adj Close'].pct_change().dropna()
+    mercado = retorno[retorno.index.weekday == 2]
+    return np.array(mercado)    
 
-    
+
+# Escolha de pasta para gravação de dados do CAPM
+
+def leitor_pasta() -> str:
+    """ Esta função executa a leitura do teclado para a escolha da pasta de gravação dos arquivos """
+    pasta_destino = input("""Digite o caminho da pasta de gravação de arquivos: """)
+    return pasta_destino
+
+
+# SAÍDA DE DADOS
+
+# bibliotecas
+import os
+import csv
+
+#Função de gravação de dados CAPM em novo arquivo .csv
+def grava_arquivo():
+    """ Esta função cria um novo arquivo CSV com os resultados a cada estimação CAPM realizada """
+    pass
