a1bbb93077cd4b73f5697127fa05a19df7a90a35
Letícia Santos da Silva <147554295+leticiasantossilva@users.noreply.github.com>
2024-01-26 02:45:06 -0300

    Add files via upload

--- Projeto 9/capm/es.py
+++ Projeto 9/capm/es.py
@@ -8,39 +8,20 @@ Data: 13/12/2023
 
 """
 
-# ENTRADA DE DADOS
-
 # Importando bibliotecas necessárias
 
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
+import requests
+import json
 import os
+import csv
 
 
-# Imput acoes
+# ENTRADA DE DADOS
 
-def leitor_acoes() -> list:
-    acoes = []
-    i = 1
-    while i < 10:
-        acoes.append(input(f'Digite o Ticker da ação que deseja avaliar {i} de 10: '))
-    
-    return acoes
-
-# Leitura do CDI
-
-def leitor_cdi(mercado) -> np.array:
-    #CDI tem que estar na mesma base do mercado (mensal, diario, semanal)
-    #CDI DIARIO
-    cdi = pd.read_json('https://api.bcb.gov.br/dados/serie/bcdata.sgs.12/dados?formato=json')
-    #CDI MES
-    cdi = pd.read_json('https://api.bcb.gov.br/dados/serie/bcdata.sgs.4390/dados?formato=json')['valor']
-    cdi1 = np.array(cdi[-len(mercado):])
-    return cdi1
 
 # Leitura de retornos de preços de ações
 
@@ -52,7 +33,6 @@ def leitor_precos(ticker:str) -> np.array:
     data_day = dado[dado.index.weekday == 2]
     fechamento = data_day['Adj Close'].pct_change().dropna()
     return np.array(fechamento)
-
 
 # Leitura de retornos da carteira de mercado (IBOVESPA)
 
@@ -66,6 +46,43 @@ def leitor_indice() -> np.array:
     return np.array(mercado)    
 
 
+# Leitor da taxa livre de risco
+
+def leitor_taxa() -> np.array:
+    """" Esta função realiza a leitura da taxa CDI nas quartas-feiras de todas as últimas 260 semanas """
+    # URL da API de CDI diário do Bacen
+    url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.12/dados?formato=json"
+    api = requests.get(url)
+    
+    # Leitura de dados em json
+    dados = api.json()
+    cdi = pd.DataFrame(dados)
+    
+    # Formatando e filtrando período
+    cdi['data'] = pd.to_datetime(cdi['data'], format='%d/%m/%Y')
+    cdi['valor'] = pd.to_numeric(cdi['valor'], errors='coerce')
+    end_date = datetime.now()
+    start_date = end_date - timedelta(weeks=268)
+    filtro = cdi[(cdi['data'] >= start_date) & (cdi['data'] <= end_date)]
+    
+    # selecionando as quarta-feiras
+    filtro.set_index('data', inplace=True)
+    dado_cdi = filtro[filtro.index.weekday == 2]
+    return np.array(dado_cdi)
+
+
+# Leitor das ações
+
+def leitor_acoes() -> list:
+    """ Função de leitura de teclado do código das 10 ações escolhidas para estimação do CAPM """
+    acoes = []
+        
+    for i in range(1, 11):
+        acoes.append(input(f'Digite o ticker da ação listada na B3 que deseja avaliar {i} de 10: (ex.: VALE3.SA) '))
+    
+    return acoes
+
+
 # Escolha de pasta para gravação de dados do CAPM
 
 def leitor_pasta() -> str:
@@ -74,13 +91,17 @@ def leitor_pasta() -> str:
     return pasta_destino
 
 
+
 # SAÍDA DE DADOS
 
-# bibliotecas
-import os
-import csv
 
 #Função de gravação de dados CAPM em novo arquivo .csv
-def grava_arquivo():
+
+def grava_csv(pasta_destino: str, resultado_capm: pd.DataFrame):
     """ Esta função cria um novo arquivo CSV com os resultados a cada estimação CAPM realizada """
-    pass
+    arquivo = resultado_capm
+    nome_arquivo = "resultados_capm.csv"
+    caminho = os.path.join(pasta_destino, nome_arquivo)
+    
+    arquivo.to_csv(caminho, index=False)
+    print(f"Resultados salvos no arquivo {nome_arquivo}")
